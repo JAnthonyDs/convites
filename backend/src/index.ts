768ef60fa633212717782e469cc88f1a21eaf1a8
Anthony D <anthonydantas15@gmail.com>
2022-07-03 11:59:25 -0300

    Servinfo imagens

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import { router } from './routes';
 import cors from 'cors'
+import path from 'path'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(router)
+
+app.use('/file',express.static(path.resolve(__dirname,'..','uploads')))
 
 const PORT = process.env.PORT || 3333
 
